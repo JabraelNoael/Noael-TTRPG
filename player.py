b35a0f7f4e5d230e5f9893d6d84e93958a9f3a5a
JabraelNoael <jabraelnoael@gmail.com>
2025-09-28 16:36:06 -0700

    updated NoaelTTRPG

--- player.py
+++ player.py
@@ -1,11 +1,5 @@
-class Player:
-    def __init__(self, name: str, age: int) -> None:
-        pass
-        self.name: str = name
-        self.age: int = age
-
+from noaelTTRPG import *
 
 Michael: Player = Player(age=18,name="Michael")
-
 
 print(Michael.name)
